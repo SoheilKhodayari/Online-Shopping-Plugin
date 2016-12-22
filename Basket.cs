e20d020438bba06e521d3cfbe304fc88ba6cbacd
Ali Vishkaie <a.vishkaie95@gmail.com>
2016-12-22 14:29:08 +0330

    basket revised

--- Basket.cs
+++ Basket.cs
@@ -9,9 +9,39 @@ namespace OnlineShopping
     public class Basket
     {
         private string _Id;
+        private DateTime _DateTime;
         private List<Item> _Items;
-        private decimal _TotalPrice;
-        private Customer _Customer;
 
+        public Basket(string id)
+        {
+            this._Id = id;
+            this._Items = new List<Item>();
+        }
+        public Basket(string id, List<Item> items)
+        {
+            this._Id = id;
+            this._Items = items;
+        }
+
+        public string getId()
+        {
+            return this._Id;
+        }
+        public void setId(string id)
+        {
+            this._Id = id;
+        }
+        public List<Item> getItems()
+        {
+            return this._Items;
+        }
+        public void addItem(Item item)
+        {
+            this._Items.Add(item);
+        }
+        public void addItems(List<Item> items)
+        {
+            this._Items.AddRange(items);
+        }
     }
 }
