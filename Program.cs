84f56ae110658562eb759ad1e2ec9d2ef2f78136
Soheil Khodayari <s_khodayari@outlook.com>
2016-12-22 14:00:39 +0330

    test case

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace OnlineShopping
     {
         static void Main(string[] args)
         {
+            ItemCategory ic = new ItemCategory("itemCategoryName");
+            NodeCategory nc = new NodeCategory("nodeCategoryName");
+            NodeCategory nc2 = new NodeCategory("nodeCategoryName2");
+            nc.addCategory(ic);
+            nc.addCategory(nc2);
+            Console.WriteLine(nc.getChildren()[1].GetType() == typeof(NodeCategory));
+
+            List<int> initialList = new List<int>();
+            initialList.Add(1);
+            // Put whatever you want in the initial list
+            List<int> listToAdd = new List<int>();
+            listToAdd.Add(2); listToAdd.Add(3);
+            // Put whatever you want in the second list
+            initialList.AddRange(listToAdd);
+            Console.WriteLine(initialList[0]);
+            Console.WriteLine(initialList[1]);
+            Console.WriteLine(initialList[2]);
+
             Console.ReadLine();
         }
     }
