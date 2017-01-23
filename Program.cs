990b75391643b5a672fe8aeb19c6e5267c095cb7
Soheil Khodayari <s_khodayari@outlook.com>
2017-01-23 14:46:15 +0330

    final changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Data.Entity.Validation;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using OnlineShopping.Interfaces;
 namespace OnlineShopping
 {
     public class test
@@ -17,82 +17,7 @@ namespace OnlineShopping
     {
         static void Main(string[] args)
         {
-            /*
-            ItemCategory ic = new ItemCategory("itemCategoryName");
-            NodeCategory nc = new NodeCategory("nodeCategoryName");
-            NodeCategory nc2 = new NodeCategory("nodeCategoryName2");
-            nc.addCategory(ic);
-            nc.addCategory(nc2);
-            Console.WriteLine(nc.getChildren()[1].GetType() == typeof(NodeCategory));
-
-            List<int> initialList = new List<int>();
-            initialList.Add(1);
-            // Put whatever you want in the initial list
-            List<int> listToAdd = new List<int>();
-            listToAdd.Add(2); listToAdd.Add(3);
-            // Put whatever you want in the second list
-            initialList.AddRange(listToAdd);
-            Console.WriteLine(initialList[0]);
-            Console.WriteLine(initialList[1]);
-            Console.WriteLine(initialList[2]);
-            */
-            
-            /*
-            Customer c = new Customer(null, "Ali", null);
-            Basket b = new Basket(null);
-            Item i = new Item(null, "name", 10, null);
-            i.setCount(10);
-            Console.WriteLine(i.getCount());
-            b.addItem(i);
-            c.addItemToBasket(i);
-            Console.WriteLine(i.getCount());
-            Console.WriteLine(b.getItems()[0].getCount());
-            */
-            //IItem i = new Item(null, "name", 10 ,10, null);
-            //IItem j = i.clone();
-            //j.incCount(1);
-            //Console.WriteLine(i.getCount());
-            //Console.WriteLine(j.getCount());
-            //i.incCount(2);
-            //Console.WriteLine(i.getCount());
-            //Console.WriteLine(j.getCount());
-            //j.incCount(5);
-            //Console.WriteLine(i.getCount());
-            //Console.WriteLine(j.getCount());
-
-
-            // serialization example
-            //test t = new test();
-            //t.name = "soheil";
-
-            //Dictionary<string, Object> dict = new Dictionary<string, Object>();
-            //Dictionary<string, Object> dict2;
-            //dict.Add("key", t);
-            //string json = JsonConvert.SerializeObject(dict, Formatting.Indented);
-            ////Console.WriteLine(json);
-            //dict2 = JsonConvert.DeserializeObject<Dictionary<string, Object>>(json);
-
-            //JObject obj = (JObject)dict2["key"];
-            //test t2 = obj.ToObject<test>();
-           // Console.WriteLine(t2.name);
-
-            //catch (DbEntityValidationException e)
-            //{
-            //    foreach (var eve in e.EntityValidationErrors)
-            //    {
-            //        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
-            //            eve.Entry.Entity.GetType().Name, eve.Entry.State);
-            //        foreach (var ve in eve.ValidationErrors)
-            //        {
-            //            Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
-            //                ve.PropertyName, ve.ErrorMessage);
-            //        }
-            //    }
-            //   // throw;
-            //}
-
-
-            // --------------------------------------simple test---------------------------------//
+           
             var db = AppContext.getInstance();
 
             Shop shop = Shop.getInstance();
@@ -100,9 +25,9 @@ namespace OnlineShopping
             shop.setId(Guid.NewGuid().ToString());
 
             Category digitalProductsCategory = new Category("DigitalProducts");
+            Category sportCategory = new Category("SportProducts");
             ItemCategory laptopCategory = new ItemCategory("Laptop");
             ItemCategory mobileCategory = new ItemCategory("Mobile");
-
 
             ItemSpec laptopItemSpec1 = new ItemSpec();
             laptopItemSpec1.addPropertyIfNotExists("color", "black");
